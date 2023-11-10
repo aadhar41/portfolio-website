2354aaf94452ef7764bbb6482362d7004d87ef86
Aadhar gaur <aadhar41@gmail.com>
2023-11-10 17:58:21 +0530

    226-Show-skill-items-at-frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,6 +10,7 @@ use App\Models\Hero;
 use App\Models\PortfolioItem;
 use App\Models\PortfolioSectionSetting;
 use App\Models\Service;
+use App\Models\SkillItem;
 use App\Models\SkillSectionSetting;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
@@ -27,7 +28,8 @@ class HomeController extends Controller
         $portfolioSectionSettings = PortfolioSectionSetting::first(['title', 'sub_title']);
         $categories = Category::all();
         $portfolioItems = PortfolioItem::all();
-        $skills = SkillSectionSetting::first();
+        $skillSection = SkillSectionSetting::first();
+        $skills = SkillItem::all();
         return view('frontend.home', compact(
             'hero',
             'typerTitles',
@@ -36,6 +38,7 @@ class HomeController extends Controller
             'portfolioSectionSettings',
             'categories',
             'portfolioItems',
+            'skillSection',
             'skills',
         ));
     }
