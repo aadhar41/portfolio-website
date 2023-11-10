86de754e1ba45119c698fc05f6d26a285ba73ef2
Aadhar gaur <aadhar41@gmail.com>
2023-11-10 14:05:57 +0530

    219-Show-dynamic-contents-in-home-page

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Http\Controllers\Admin\PortfolioSectionSettingController;
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\Category;
 use App\Models\Hero;
+use App\Models\PortfolioItem;
+use App\Models\PortfolioSectionSetting;
 use App\Models\Service;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
@@ -18,7 +22,17 @@ class HomeController extends Controller
         $typerTitles = TyperTitle::pluck('title');
         $services = Service::pluck('description','name');
         $about = About::first(['title','description','image','resume']);
-        // dd($about);
-        return view('frontend.home', compact('hero', 'typerTitles', 'services', 'about'));
+        $portfolioSectionSettings = PortfolioSectionSetting::first(['title','sub_title']);
+        $categories = Category::all();
+        $portfolioItems = PortfolioItem::all();
+        return view('frontend.home', compact(
+            'hero', 
+            'typerTitles', 
+            'services', 
+            'about', 
+            'portfolioSectionSettings',
+            'categories',
+            'portfolioItems',
+        ));
     }
 }
--- resources/views/frontend/sections/portfolio.blade.php
+++ resources/views/frontend/sections/portfolio.blade.php
@@ -3,10 +3,11 @@
         <div class="row">
             <div class="col-lg-6 offset-lg-3 text-center">
                 <div class="section-title">
-                    <h3 class="title">Latest Portfolio</h3>
+                    <h3 class="title">{!! $portfolioSectionSettings->title !!}</h3>
                     <div class="desc">
-                        <p>Earum quos animi numquam excepturi eveniet explicabo repellendus rem esse. Quae quasi
-                            odio enim.</p>
+                        <p>
+                            {!! $portfolioSectionSettings->sub_title !!}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -15,30 +16,31 @@
             <div class="col-sm-12">
                 <ul class="filter-menu">
                     <li class="active" data-filter="*">All Projects</li>
-                    <li data-filter=".branding">Branding Design</li>
-                    <li data-filter=".interface">User Interface</li>
-                    <li data-filter=".experience">User Experience</li>
-                    <li data-filter=".development">Web Development</li>
+                    @foreach($categories as $category)
+                    <li data-filter=".{{$category->slug}}">{{$category->name}}</li>
+                    @endforeach
                 </ul>
             </div>
         </div>
         <div class="portfolio-wrapper">
             <div class="row portfolios">
-                <div data-wow-delay="0.3s" class="col-md-6 col-lg-4 filter-item branding">
-                    <div class="single-portfolio">
-                        <figure class="portfolio-image">
-                            <img src="{{asset('frontend/images/portfolio-1.jpg')}}" alt="">
-                        </figure>
-                        <div class="portfolio-content">
-                            <a href="{{asset('frontend/images/portfolio-1.jpg')}}" data-lity class="icon"><i class="fas fa-plus"></i></a>
-                            <h4 class="title"><a href="javascript:void(0);">Black Golden</a></h4>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                @foreach ($portfolioItems as $item)
+                    <div data-wow-delay="0.3s" class="col-md-6 col-lg-4 filter-item {{$item->category->slug}}">
+                        <div class="single-portfolio">
+                            <figure class="portfolio-image">
+                                <img src="{{$item->image}}" alt="{{$item->title}}">
+                            </figure>
+                            <div class="portfolio-content">
+                                <a href="{{$item->image}}" data-lity class="icon"><i class="fas fa-plus"></i></a>
+                                <h4 class="title"><a href="javascript:void(0);">{{$item->title}}</a></h4>
+                                <div class="desc">
+                                    <p>{!! Str::limit($item->description, 100); !!}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div data-wow-delay="0.4s" class="col-md-6 col-lg-4 filter-item interface">
+                @endforeach
+                {{-- <div data-wow-delay="0.4s" class="col-md-6 col-lg-4 filter-item interface">
                     <div class="single-portfolio">
                         <figure class="portfolio-image">
                             <img src="{{asset('frontend/images/portfolio-2.jpg')}}" alt="">
@@ -149,7 +151,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
