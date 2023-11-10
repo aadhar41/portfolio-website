bf26d8446e5025dfcb8c9691cab485066209b786
Aadhar gaur <aadhar41@gmail.com>
2023-11-10 23:44:27 +0530

    229-Handling-form-data-and-storing

---
+++ resources/views/admin/experience/create.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layout')
+
+@section('content')
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{route('admin.experience.update', 1)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">{{__('Create Experience Content')}}</h3>
+                            <div class="card-tools">
+                                <a href="{{route('admin.experience.index')}}" class="mr-3"><i class="fa fa-list" aria-hidden="true"></i> {{__('Lists')}}</a>
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="title">{{__('Title')}}</label>
+                                <input class="form-control" id="title" name="title" type="text" placeholder="{{__('Title')}}" value="{{old('title', $data->title)}}" />
+                            </div>
+
+                            <div class="form-group">
+                                <label>{{__('Description')}}</label>
+                                <textarea class="form-control summernote" rows="3" id="description" name="description" placeholder="{{__('Description')}}">{!! old('description', $data->description) !!}</textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">{{__('Phone')}}</label>
+                                <input class="form-control" id="phone" name="phone" type="text" placeholder="{{__('Phone')}}" value="{{old('phone', $data->phone)}}" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">{{__('Email')}}</label>
+                                <input class="form-control" id="email" name="email" type="email" placeholder="{{__('Email')}}" value="{{old('email', $data->email)}}" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="image">{{__('Image')}}</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" name="image" class="custom-file-input" id="image">
+                                        <label class="custom-file-label" for="image">{{__('Choose Image')}}...</label>
+                                    </div>
+                                </div>
+                            </div>
+                            @if (isset($data->image))
+                            <div class="form-group">
+                                <img src="{{asset($data->image)}}" class="img-fluid img-thumbnail ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" height="150" width="230" alt="Image">
+                            </div>
+                            @endif
+ 
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">
+                                {{__('Update')}}
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.card -->
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ExperienceController;
 use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\PortfolioItemController;
 use App\Http\Controllers\Admin\PortfolioSectionSettingController;
@@ -86,4 +87,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
 
     // Skills Item Routes
     Route::resource('skill-item', SkillItemController::class);
+
+    // Experience Routes
+    Route::resource('experience', ExperienceController::class);
 });
