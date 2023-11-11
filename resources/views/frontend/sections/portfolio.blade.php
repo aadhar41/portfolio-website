9b75736678f9a4e0f534c3ad0f5585ffdd629f59
Aadhar gaur <aadhar41@gmail.com>
2023-11-11 10:44:29 +0530

    235-Show-dynamic-contents-in-frontend

--- resources/views/frontend/sections/portfolio.blade.php
+++ resources/views/frontend/sections/portfolio.blade.php
@@ -2,44 +2,74 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 offset-lg-3 text-center">
-                <div class="section-title">
-                    <h3 class="title">{!! $portfolioSectionSettings->title !!}</h3>
-                    <div class="desc">
-                        <p>
-                            {!! $portfolioSectionSettings->sub_title !!}
-                        </p>
+                @if ($portfolioSectionSettings)
+                    <div class="section-title">
+                        <h3 class="title">{!! $portfolioSectionSettings->title !!}</h3>
+                        <div class="desc">
+                            <p>
+                                {!! $portfolioSectionSettings->sub_title !!}
+                            </p>
+                        </div>
                     </div>
-                </div>
+                @else
+                    <div class="section-title">
+                        <h3 class="title">Latest Portfolio</h3>
+                        <div class="desc">
+                            <p>Earum quos animi numquam excepturi eveniet explicabo repellendus rem esse. Quae quasi
+                                odio enim.</p>
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
         <div class="row">
             <div class="col-sm-12">
                 <ul class="filter-menu">
                     <li class="active" data-filter="*">All Projects</li>
-                    @foreach($categories as $category)
-                    <li data-filter=".{{$category->slug}}">{{$category->name}}</li>
-                    @endforeach
+                    @if ($categories)                        
+                        @foreach($categories as $category)
+                            <li data-filter=".{{$category->slug}}">{{$category->name}}</li>
+                        @endforeach
+                    @endif
                 </ul>
             </div>
         </div>
         <div class="portfolio-wrapper">
             <div class="row portfolios">
-                @foreach ($portfolioItems as $item)
-                    <div data-wow-delay="0.3s" class="col-md-6 col-lg-4 filter-item {{$item->category->slug}}">
+                @if ($portfolioItems)
+                    @foreach ($portfolioItems as $item)
+                        <div data-wow-delay="0.3s" class="col-md-6 col-lg-4 filter-item {{$item->category->slug}}">
+                            <div class="single-portfolio">
+                                <figure class="portfolio-image">
+                                    <img src="{{$item->image}}" alt="{{$item->title}}">
+                                </figure>
+                                <div class="portfolio-content">
+                                    <a href="{{$item->image}}" data-lity class="icon"><i class="fas fa-plus"></i></a>
+                                    <h4 class="title"><a href="{{route('show.portfolio', $item->id)}}">{{$item->title}}</a></h4>
+                                    <div class="desc">
+                                        <p>{!! Str::limit(strip_tags($item->description), 100); !!}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div data-wow-delay="0.5s" class="col-md-6 col-lg-4 filter-item development">
                         <div class="single-portfolio">
                             <figure class="portfolio-image">
-                                <img src="{{$item->image}}" alt="{{$item->title}}">
+                                <img src="{{asset('images/portfolio-9.jpg')}}" alt="Img">
                             </figure>
                             <div class="portfolio-content">
-                                <a href="{{$item->image}}" data-lity class="icon"><i class="fas fa-plus"></i></a>
-                                <h4 class="title"><a href="{{route('show.portfolio', $item->id)}}">{{$item->title}}</a></h4>
+                                <a href="{{asset('images/portfolio-9.jpg')}}" data-lity class="icon"><i
+                                        class="fas fa-plus"></i></a>
+                                <h4 class="title"><a href="portfolio-details.html">Branding Design</a></h4>
                                 <div class="desc">
-                                    <p>{!! Str::limit(strip_tags($item->description), 100); !!}</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
     </div>
--- resources/views/frontend/sections/skills.blade.php
+++ resources/views/frontend/sections/skills.blade.php
@@ -4,27 +4,44 @@
             <div class="col-lg-6">
                 <div class="row">
                     <div class="col-sm-12">
-                        <div class="section-title">
-                            <h3 class="title">{!! $skillSection->title !!}</h3>
-                            <div class="desc">
-                                <p>{!! $skillSection->sub_title !!}</p>
+                        @if ($skillSection)
+                            <div class="section-title">
+                                <h3 class="title">{!! $skillSection->title !!}</h3>
+                                <div class="desc">
+                                    <p>{!! $skillSection->sub_title !!}</p>
+                                </div>
                             </div>
-                        </div>
+                        @else
+                            <div class="section-title">
+                                <h3 class="title">Skills</h3>
+                                <div class="desc">
+                                    <p>Earum quos animi numquam excepturi eveniet explicabo repellendus rem esse.
+                                        Quae quasi
+                                        odio enim.</p>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
                 <div class="row skills">
-                    @foreach ($skills as $skill)
-                    <?php 
-                        $num = rand(0, 5);
-                        $color = array('#558bff', '#fecc90', '#ff885e', '#282828', '#190844', '#9dd3ff');
-                    ?>
+                    @if ($skills)
+                        @foreach ($skills as $skill)
+                        <div class="col-sm-6">
+                            <div class="bar_group wow fadeInUp" data-wow-delay="0.3s" data-max="100">
+                                <div class="title">{!! $skill->name !!}</div>
+                                <div class="bar_group__bar thick elastic" data-value="{!! $skill->percent !!}" data-color="{{getColor($loop->index)}}" data-tooltip="true" data-show-values="false" data-unit="%"></div>
+                            </div>
+                        </div>
+                        @endforeach
+                    @else
                     <div class="col-sm-6">
                         <div class="bar_group wow fadeInUp" data-wow-delay="0.3s" data-max="100">
-                            <div class="title">{!! $skill->name !!}</div>
-                            <div class="bar_group__bar thick elastic" data-value="{!! $skill->percent !!}" data-color="{{getColor($loop->index)}}" data-tooltip="true" data-show-values="false" data-unit="%"></div>
+                            <div class="title">Web Developement</div>
+                            <div class="bar_group__bar thick elastic" data-value="70" data-color="#ff885e"
+                                data-tooltip="true" data-show-values="false" data-unit="%"></div>
                         </div>
                     </div>
-                    @endforeach
+                    @endif
                 </div>
             </div>
             <div class="col-lg-6 d-none d-lg-block">
