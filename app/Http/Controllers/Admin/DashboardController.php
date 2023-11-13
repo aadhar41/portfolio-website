37242f7551d0b4aaa29f2193e01f822270cecd76
Aadhar gaur <aadhar41@gmail.com>
2023-11-13 18:57:51 +0530

    259-Show-dynamic-data-at-dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Feedback;
+use App\Models\PortfolioItem;
+use App\Models\Service;
+use App\Models\SkillItem;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard');
+        $blogCount = Blog::count();
+        $skillCount = SkillItem::count();
+        $portfolioCount = PortfolioItem::count();
+        $feedbackCount = Feedback::count();
+        $serviceCount = Service::count();
+        return view('dashboard', compact('blogCount','skillCount','portfolioCount','feedbackCount','serviceCount'));
     }
 }
